3f6f7511aa171a7e22a3a59497922184922dad6b
eric <eric@pc.lan>
2025-10-08 16:14:19 -0400

    added menu and results

--- src/clevelandelectionmap.cr
+++ src/clevelandelectionmap.cr
@@ -4,17 +4,17 @@ require "base64"
 require "json"
 
 # load the data for the 2025 primary election
-#primaryinput2025 = File.read_lines("data/2025primary.txt")
+# primaryinput2025 = File.read_lines("data/2025primary.txt")
 #
-#primary2025 = Hash(String, Hash(String, Int32)).new
+# primary2025 = Hash(String, Hash(String, Int32)).new
 #
-#linecounter = 0
-#truecounter = 0
-#name = ""
-#temp = Hash(String, Int32).new
-#names = [] of String
-#votes = [] of Int32
-#primaryinput2025.each do |line|
+# linecounter = 0
+# truecounter = 0
+# name = ""
+# temp = Hash(String, Int32).new
+# names = [] of String
+# votes = [] of Int32
+# primaryinput2025.each do |line|
 #  if line.starts_with?("Precinct")
 #    if truecounter != 0
 #      (0..names.size - 1).each do |n|
@@ -22,15 +22,15 @@ require "json"
 #      end
 #    end
 
-    # reset
+# reset
 #    primary2025[name] = temp
 #    temp = Hash(String, Int32).new
 #    names = [] of String
 #    votes = [] of Int32
-    # temp = Hash(String, Int32).new
+# temp = Hash(String, Int32).new
 #    linecounter = 0
 #    name = line[-4..]
-    # convert name to format used in data
+# convert name to format used in data
 #    name = name.delete('-')
 #    if name[0] == '0'
 #      name = name.delete_at(0)
@@ -46,18 +46,18 @@ require "json"
 #
 #  truecounter += 1
 #  linecounter += 1
-#end
+# end
 
 # puts primary2025
-#primary2025json = primary2025.to_json
+# primary2025json = primary2025.to_json
 primary2025json = File.read("data/primary2025.json")
 # puts primary2025json
 
 server = HTTP::Server.new do |context|
   case context.request.path
   when "/"
-    context.response.redirect("/electionmap")
-  when "/electionmap"
+    context.response.redirect("/electionmap/2025primary")
+  when "/electionmap/2025primary"
     io = IO::Memory.new
     render_page = ECR.embed "pages/primary2025.html.ecr", io
     context.response.content_type = "text/html"
