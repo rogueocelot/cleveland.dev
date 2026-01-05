8091be32dff35deed802d6fe2e157bacd7654d79
eric <ericpasho1@gmail.com>
2026-01-05 18:51:18 -0500

    added 2025 general

--- src/clevelandelectionmap.cr
+++ src/clevelandelectionmap.cr
@@ -3,20 +3,30 @@ require "ecr"
 require "base64"
 require "json"
 
-#prerender page for 2025 primary
-primary2025json = File.read("data/primary2025.json")
-p2025io = IO::Memory.new
-p2025render_page = ECR.embed "pages/primary2025.html.ecr", p2025io
+#prerender page for 2025 council primary
+primary2025json = File.read("data/councilprimary2025.json")
+councilprimary2025io = IO::Memory.new
+render_page = ECR.embed "pages/councilprimary2025.html.ecr", councilprimary2025io
+
+#prerender page for 2025 council general
+councilgeneral2025json = File.read("data/councilgeneral2025.json")
+councilgeneral2025io = IO::Memory.new
+render_page = ECR.embed "pages/councilgeneral2025.html.ecr", councilgeneral2025io
 
 server = HTTP::Server.new do |context|
   case context.request.path
   when "/"
-    context.response.redirect("/electionmap/2025primary")
+    context.response.redirect("/electionmap/council/primary/2025")
   when "/electionmap/"
-    context.response.redirect("/electionmap/2025primary")
-  when "/electionmap/2025primary"
+    context.response.redirect("/electionmap/council/primary/2025")
+  when "/electionmap/council/"
+    context.response.redirect("/electionmap/council/primary/2025")
+  when "/electionmap/council/primary/2025"
     context.response.content_type = "text/html"
-    context.response.print(p2025io)
+    context.response.print(councilprimary2025io)
+  when "/electionmap/council/general/2025"
+    context.response.content_type = "text/html"
+    context.response.print(councilgeneral2025io)
   end
 end
 
